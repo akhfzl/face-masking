0df8320caff103fd6493f3a04556f2a68f5144f9
akhfzl <akhfaiz14@gmail.com>
2024-09-28 13:06:49 +0700

    push_28_09

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from utilization import read_csv, collect_crop_images
+# from utilization import read_csv, collect_crop_images
+from dataprep import DataPreparation
 
 
 if __name__ == '__main__':
-    df = read_csv('link_prepare.csv')
-    saving = collect_crop_images(df)
+    dataprep = DataPreparation('extracted_images')
+    link = dataprep.link_recursive('extracted_images')
+    print(link)
+    # df = read_csv('link_prepare.csv')
+    # saving = collect_crop_images(df)
 
     print("Cropped images have been saved.")
